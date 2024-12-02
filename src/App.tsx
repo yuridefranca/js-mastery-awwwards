c54ea9d6c0532fe66bf8fe3f3d0798e480862b5d
Yuri Serafim <yuridefranca@gmail.com>
2024-12-02 18:29:49 -0300

    :tada: initialize React, TypeScript, and Vite setup with ESLint and Tailwind CSS configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+	return (
+		<main>
+			<h1 className='text-5xl text-violet-300 font-bold'>Welcome to Awwwards</h1>
+		</main>
+	);
+};
+
+export default App;
