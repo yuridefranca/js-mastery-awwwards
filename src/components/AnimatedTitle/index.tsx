59ce45ec7eaa80a6aef9603a5c32f8b26359c8f1
Yuri Serafim <yuridefranca@gmail.com>
2024-12-05 22:35:50 -0300

    fix: :bug: correct className in AnimatedTitle component for proper flex wrapping

--- src/components/AnimatedTitle/index.tsx
+++ src/components/AnimatedTitle/index.tsx
@@ -46,7 +46,7 @@ export const AnitmatedTitle = ({ title, containerClassName }: AnitmatedTitleProp
 			{title.split('<br />').map((line, index) => (
 				<div
 					key={index}
-					className="flex-center max-w-full gap-2 px-10 md:gap:3"
+					className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap:3"
 				>
 					{line.split(' ').map((word, i) => (
 						<span
